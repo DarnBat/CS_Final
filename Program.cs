df34580ac3d2d40cad4f6c383e8d95fc2dea48f1
Vladislav.M <lidpid@gmail.com>
2023-02-02 00:56:22 +0600

    program completed

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int GetOutputLength(string[] input)
+{
+    int x = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3) x++;
+    }
+    return x;
+}
+string[] CompileOutput(string[] input)
+{
+    string[] output = new string[GetOutputLength(input)];
+    int j = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= 3)
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+Console.Clear();
+Console.WriteLine("Введите элементы массива через пробел");
+string[] input = Console.ReadLine().Split(' ');
+Console.WriteLine($"[{String.Join(' ', CompileOutput(input))}]");
